4fd7ed0ffef1ec848aa3b929781fbd1d26c362a8
vagisha <vagisha.root@gmail.com>
2025-01-20 16:14:32 +0530

    add service

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,33 +1,31 @@
 import React from 'react'
-import inflact from "../assets/inflact.svg"
 import { Link } from 'react-router-dom'
-import snapInsta from "../assets/snapInsta.svg"
+import serviceJson from "../json/services.json"
 
 const Services = () => {
   return (
     <div className="w-full flex flex-col items-center bg-white">
         <div className="text-center font-bold text-3xl">All Services</div>
-        <div id="socialMedia" className="flex flex-col gap-y-[15px] ">
-            <div className="flex flex-col w-full items-start gap-y-[15px]">
-                <div className="instagram font-bold underline text-2xl w-full px-[20px] bg-slate-200 py-[15px] rounded-t-xl">Instagram Video Download</div>
-                <div className="grid grid-cols-4 gap-x-[15px]">
-                    <div className="flex flex-col gap-y-[10px] w-[300px] items-start border border-[#0a1743] p-[5px] rounded-lg">
-                        <div className="w-full bg-slate-50">
-                            <img src={inflact} className='w-[80%] h-[200px]' />
+        {serviceJson.map((v,i)=>
+            <div id={v.id} className="flex flex-col gap-y-[15px]" key={i}>
+            {v.types.map((t,ind)=>
+                <div className="flex flex-col w-full items-start gap-y-[15px]" key={ind}>
+                    <div className="instagram font-bold underline text-2xl w-full px-[20px] bg-slate-200 py-[15px] rounded-t-xl">{t.name}</div>
+                    <div className="grid grid-cols-4 gap-x-[15px]">
+                    {t.obj.map((s,ind)=>
+                        <div className="flex flex-col gap-y-[10px] w-[300px] items-start border border-[#0a1743] p-[5px] rounded-lg" key={s.key}>
+                            <div className="w-full bg-slate-50">
+                                <img src={s.svg} className='w-[80%] h-[200px]' />
+                            </div>
+                            <Link className="bg-green-700 px-[10px] py-[5px] rounded text-white" target='_blank'>See Tutorial</Link>
+                            <Link className="bg-[#0a1743] px-[10px] py-[5px] rounded text-white" target="_blank">Go for Download</Link>
                         </div>
-                        <Link className="bg-green-700 px-[10px] py-[5px] rounded text-white" target='_blank'>See Tutorial</Link>
-                        <Link className="bg-[#0a1743] px-[10px] py-[5px] rounded text-white" target="_blank">Go for Download</Link>
-                    </div>
-                    <div className="flex flex-col gap-y-[10px] w-[300px] items-start border border-[#0a1743] p-[5px] rounded-lg">
-                        <div className="w-full bg-slate-50">
-                            <img src={snapInsta} className='w-[80%] h-[200px]' />
-                        </div>
-                        <Link className="bg-green-700 px-[10px] py-[5px] rounded text-white" target='_blank'>See Tutorial</Link>
-                        <Link className="bg-[#0a1743] px-[10px] py-[5px] rounded text-white" target="_blank">Go for Download</Link>
+                    )}
                     </div>
                 </div>
+            )}
             </div>
-        </div>
+        )}
     </div>
   )
 }
